8f66cf25782b19e90da6613bf1f045d41939ab39
Matt McManus <matt@mcmanus.io>
2016-08-13 16:08:27 -0400

    ember init ember-cli 2.7.0

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -3,7 +3,8 @@ const { Router: EmberRouter } = Ember;
 import config from './config/environment';
 
 const Router = EmberRouter.extend({
-  location: config.locationType
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
 Router.map(function() {
